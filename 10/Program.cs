f72b8fcc62a7e383e08aeb6a20020980e0b87733
Denis <i9852829292@yandex.ru>
2022-11-17 13:41:58 +0100

    попытка1

---
+++ 10/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 10: Напишите программу, 
+// которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа. 
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine ("ВВедите трех значное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number<1000 && number>99)
+{
+    
+}
+Console.WriteLine("Вы ввели не трех значное число ");
+
+int ToEat (int num)
+{       
+  int res = num / 10 % 10;   
+  return res;
+}
+int result = ToEat(number);
+
+Console.WriteLine(result);
---
+++ 13/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 13: Напишите программу, 
+// которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. 
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("ВВедите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Three(int num1)
+{
+    int result = 0;
+    if (num1 < 100);
+    return num1;
+    if (num1 > 999);
+    {
+        while (num1 < 10000)
+        {
+            num1 = num1 / 10;
+        }
+        result = num1 % 10;
+        return result;
+    }
+}
+int enter = Three(number);
+Console.WriteLine(Three(enter));
